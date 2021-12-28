334ce9fa52104a73bf7e408d500bf49da3ef67c2
patrickiyog <patrickiyog97@gmail.com>
2021-12-28 16:35:55 +1300

    Setup project in React and TypeScript

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # weather-app
+
+This is a work in progress.
---
+++ src/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(
+    <div>Weather app by patrickiyog</div>,
+    document.getElementById('root')
+);
