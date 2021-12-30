a45e71de37761f2581d964c5d44a4dc9ce86195d
patrickiyog <patrickiyog97@gmail.com>
2021-12-31 09:36:00 +1300

    create weather store for state management

---
+++ client/src/WeatherStore.ts
@@ -0,0 +1,31 @@
+import create from "zustand/react";
+
+// Project Imports
+import { Weather } from "./models/Weather";
+
+interface WeatherState {
+    weather: Weather;
+    changeWeather: (weather: Weather) => void;
+};
+
+export const weatherStore = create<WeatherState>(set => ({
+    weather: {
+        id: '',
+        country: '',
+        city: '',
+        main: '',
+        description: '',
+        icon: '',
+        temp: 0,
+        feelsLike: 0,
+        high: 0,
+        low: 0,
+        pressure: 0,
+        humidity: 0,
+        sunrise: 0,
+        sunset: 0
+    },
+    changeWeather: (weather: Weather) => {
+        set(() => ({ weather: weather }));
+    }
+}));
